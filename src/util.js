5204f50bfcc0498fb8c0b38ae34369f503ac4612
Mohammed Yehia <m-ya@windowslive.com>
2018-04-05 16:07:37 +0300

    fixing the search

--- src/util.js
+++ src/util.js
@@ -1,18 +1,14 @@
 var countries = require('./country.json')
 
 
-function returnList(input){
-  var tempArr = countries[input].countries.map(x=>x.country)
-return tempArr;
-}
-
-var result;
 function search(name){
+
   if(name){
   let newName = name.slice(1).toLowerCase();
   let firstLetter = name[0].toUpperCase()
-  result = returnList(firstLetter).filter(country => country.startsWith(firstLetter+newName));
-  return result
+  var countriesArr = countries[firstLetter].countries.map(x=>x.country)
+  return countriesArr.filter(country => country.startsWith(firstLetter+newName));
+
   }
 }
 
