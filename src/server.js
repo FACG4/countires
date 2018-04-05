acb3edd632f17f47a8d554924233422e260ebc77
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-04-05 16:37:33 +0300

    portttt

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 let http = require('http');
 var handler = require('./handler');
 let server = http.createServer(handler);
-const port = process.env.port || 3000;
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`this server listens on port: ${port}`);
